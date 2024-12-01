5042d73d51728b56e02fd6aebd2a3289ba49d4ad
Chris Paterson <chris@chrispaterson.co.uk>
2024-12-01 20:04:36 +0000

    Day 1

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"log/slog"
+	"os"
+
+	runner "github.com/ThePants999/advent-of-code-go-runner"
+)
+
+func main() {
+	lvl := new(slog.LevelVar)
+	lvl.Set(slog.LevelWarn)
+	logger := slog.New(slog.NewTextHandler(os.Stderr, &slog.HandlerOptions{Level: lvl}))
+
+	r := runner.NewRunner(logger, "2024", []runner.DayImplementation{Day1})
+	r.Run()
+}

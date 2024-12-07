f9dc20023f3aa8f92ba87db65bb25181920d8bdb
Chris Paterson <chris@chrispaterson.co.uk>
2024-12-07 15:00:16 +0000

    Day 7 - just use recursion, dumbass

--- 7.go
+++ 7.go
@@ -7,7 +7,6 @@ import (
 	"strings"
 
 	runner "github.com/ThePants999/advent-of-code-go-runner"
-	iterium "github.com/mowshon/iterium"
 )
 
 var Day7 = runner.DayImplementation{
@@ -32,14 +31,6 @@ type equation struct {
 	operands []int
 }
 
-type operator int
-
-const (
-	OP_ADD operator = iota
-	OP_MULTIPLY
-	OP_CONCAT
-)
-
 func Day7Part1(logger *slog.Logger, input string) (string, any) {
 	numbers := strings.Fields(input)
 	equations := make([]*equation, 0, 1000)
@@ -61,20 +52,26 @@ func Day7Part1(logger *slog.Logger, input string) (string, any) {
 		}
 	}
 
-	sum := runTest(equations, []operator{OP_ADD, OP_MULTIPLY})
+	sum := runTest(equations, false)
 	return strconv.Itoa(sum), equations
 }
 
 func Day7Part2(logger *slog.Logger, input string, part1Context any) string {
 	equations := part1Context.([]*equation)
-	sum := runTest(equations, []operator{OP_ADD, OP_MULTIPLY, OP_CONCAT})
+	sum := runTest(equations, true)
 	return strconv.Itoa(sum)
 }
 
-func runTest(equations []*equation, operators []operator) int {
+func runTest(equations []*equation, allowConcatenation bool) int {
 	c := make(chan int)
 	for _, eq := range equations {
-		go testEquation(eq, operators, c)
+		go func() {
+			if testEquation(eq, eq.operands[0], 1, allowConcatenation) {
+				c <- eq.result
+			} else {
+				c <- 0
+			}
+		}()
 	}
 
 	sum := 0
@@ -85,35 +82,33 @@ func runTest(equations []*equation, operators []operator) int {
 	return sum
 }
 
-func testEquation(eq *equation, operators []operator, c chan int) {
-	combinations := iterium.Product(operators, len(eq.operands)-1)
-	for {
-		combination, err := combinations.Next()
-		if err != nil {
-			break
-		}
+func testEquation(eq *equation, value int, index int, allowConcatenation bool) bool {
+	if index == len(eq.operands) {
+		return eq.result == value
+	}
 
-		value := eq.operands[0]
-		for i := 0; i < len(combination); i++ {
-			switch combination[i] {
-			case OP_ADD:
-				value += eq.operands[i+1]
-			case OP_MULTIPLY:
-				value *= eq.operands[i+1]
-			case OP_CONCAT:
-				numDigits := math.Log10(float64(eq.operands[i+1])) + 1
-				value *= int(math.Pow10(int(numDigits)))
-				value += eq.operands[i+1]
-			}
-			if value > eq.result {
-				break
-			}
-		}
-		if value == eq.result {
-			c <- eq.result
-			return
+	if value > eq.result {
+		return false
+	}
+
+	newValue := value + eq.operands[index]
+	if testEquation(eq, newValue, index+1, allowConcatenation) {
+		return true
+	}
+
+	newValue = value * eq.operands[index]
+	if testEquation(eq, newValue, index+1, allowConcatenation) {
+		return true
+	}
+
+	if allowConcatenation {
+		numDigits := math.Log10(float64(eq.operands[index])) + 1
+		newValue = value * int(math.Pow10(int(numDigits)))
+		newValue += eq.operands[index]
+		if testEquation(eq, newValue, index+1, allowConcatenation) {
+			return true
 		}
 	}
 
-	c <- 0
+	return false
 }
--- go.mod
+++ go.mod
@@ -2,13 +2,14 @@ module github.com/ThePants999/advent-of-code-2024
 
 go 1.23.3
 
-require github.com/ThePants999/advent-of-code-go-runner v0.2.0
+require (
+	github.com/ThePants999/advent-of-code-go-runner v0.2.0
+	github.com/deckarep/golang-set/v2 v2.7.0
+)
 
 require (
-	github.com/deckarep/golang-set/v2 v2.7.0 // indirect
 	github.com/logrusorgru/aurora/v4 v4.0.0 // indirect
-	github.com/mowshon/iterium v1.0.0 // indirect
-	golang.org/x/exp v0.0.0-20230307190834-24139beb5833 // indirect
+	github.com/stretchr/testify v1.8.2 // indirect
 )
 
 replace github.com/ThePants999/advent-of-code-go-runner v0.2.0 => ../advent-of-code-go-runner
